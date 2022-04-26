1e24395d4b9c476c3572b51ee5c02a6fada30200
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2022-04-26 15:28:58 +0530

    Add files via upload

---
+++ Javascript/Day1/index.js
@@ -0,0 +1,54 @@
+// Refer the documentation
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+/* var myName = "Gargi"; */
+// case sensitive
+// console.log(MyName);
+
+// var name can't start with a number
+//var 1myName="Gargi";
+
+// var name can start with _ , $
+
+// correct var $myname="gargi";
+// correctvar _myname="gargi";
+// wrong var 1myname="gargi";
+
+// car name can not spaces in between
+// we use camelcasing
+
+/* var myname = "gargi"; */
+
+// datatypes
+
+/* primitive datatypes
+number , string, boolean,undefined */
+
+//------------------------------------------
+//number datatype
+/* var mymarks = 90; */
+
+//boolean
+/* var myname = false; */
+// type is returning the datatype of my variable
+/* console.log(typeof myname); */
+
+var myCar = null;
+/* console.log(mycar); */
+
+/* var myCycle;
+console.log(myCycle);
+ */
+/* 
+NaN- Not a number */
+
+var myMarks = "90";
+console.log(isNaN(myMarks));
+
+/* Gargi  - is not a number // true */
+/* 90  - is not a number // false */
+/* "90"  - is not a number // true */
+
+/* boolean is a datatype  */
+
+/* isNaN is a function in JS that tells wheather a variable ia a number or not */
